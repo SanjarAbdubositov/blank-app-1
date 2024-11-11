40c472b85c5cc02dadc3e9e189446c511e76ef02
SanjarAbdubositov <sanjarabdubositov011@gmail.com>
2024-11-11 13:41:33 +0000

    works

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+background_css = '''
+    <style>
+    .stApp {
+        background-image: url("https://aiesec.ba/wp-content/uploads/2020/10/Thumbnail-Praksa-Global-Teacher-AIESEC-u-Bosni-i-Hercegovini.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        color: white;
+    }
+    </style>
+'''
